3423bd299dee0924cd718b42c8c644627c09f3b0
juancra264 <juancra264@hotmail.com>
2019-06-21 12:41:52 -0500

    update 20190621

--- get_Info_k8s.sh
+++ get_Info_k8s.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 #command="kubectl get pods --namespace=kubedemo"
-command="kubectl get pods"
+command="kubectl get pods --all-namespaces"
 echo $command
 $command
 
 command="kubectl get services"
+echo $command
+$command
+
+command="kubectl get nodes"
+echo $command
+$command
+
+command="kubectl get namespaces"
 echo $command
 $command
